8b702718403656101b322d32460e1fc23940045e
Owen Jones <owen.jones2@uk.baesystems.com>
2022-12-16 09:53:52 +0000

    Fixing vars populated in Remote to agree with pipeline YAML

--- main.tf
+++ main.tf
@@ -42,26 +42,25 @@ module "remote" {
   resource_group           = module.context.resource_group
   azdo_project_name        = var.azdo_project_name
   azdo_variable_group_name = var.azdo_variable_group_name
+  azdo_service_connection  = var.azdo_service_connection
 
   vg_secret_vars = {
     azdo_pat = "${var.azdo_pat}",
   }
 
   vg_vars = {
-    "${var.base_name}_install_remote" = "true" ,
+    "create_acr" = "${var.create_acr}",
+    "create_law" = "${var.create_law}",
+    "create_managed_identity" = "${var.create_managed_identity}",
 
-    "${var.base_name}_create_acr" = "${var.create_acr}",
-    "${var.base_name}_create_law" = "${var.create_law}",
-    "${var.base_name}_create_managed_identity" = "${var.create_managed_identity}",
+    "create_vnet" = "${var.create_vnet}",
+    "existing_vnet_name" = "${var.existing_vnet_name}",
+    "new_vnet_address_space" = "${var.new_vnet_address_space}",
+    "existing_vnet_rg_location" = "${var.existing_vnet_rg_location}",
+    "existing_vnet_rg_name" = "${var.existing_vnet_rg_name}",
 
-    "${var.base_name}_create_vnet" = "${var.create_vnet}",
-    "${var.base_name}_existing_vnet_name" = "${var.existing_vnet_name}",
-    "${var.base_name}_new_vnet_address_space" = "${var.new_vnet_address_space}",
-    "${var.base_name}_existing_vnet_rg_location" = "${var.existing_vnet_rg_location}",
-    "${var.base_name}_existing_vnet_rg_name" = "${var.existing_vnet_rg_name}",
-
-    "${var.base_name}_create_subnet" = "${var.create_subnet}",
-    "${var.base_name}_new_subnet_address_prefixes" = "${var.new_subnet_address_prefixes}",
-    "${var.base_name}_existing_subnet_id" = "${var.existing_subnet_id}",
+    "create_subnet" = "${var.create_subnet}",
+    "new_subnet_address_prefixes" = "${var.new_subnet_address_prefixes}",
+    "existing_subnet_id" = "${var.existing_subnet_id}",
   } 
 }
