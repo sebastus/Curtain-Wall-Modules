6fa4f45632763d5e4cfb1f029934165b0e0e9bb3
Greg Oliver <golive@microsoft.com>
2023-02-20 18:12:40 +0000

    pipeline

--- remote/setenv.sh
+++ remote/setenv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "##vso[task.setvariable variable=TF_VAR_azdo_org_name]${{ variables.azdo_org_name }}"
-echo "##vso[task.setvariable variable=TF_VAR_azdo_pat]${{ variables.azdo_pat }}"
+# echo "##vso[task.setvariable variable=TF_VAR_azdo_pat]${{ variables.azdo_pat }}"
