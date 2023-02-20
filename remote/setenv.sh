788147501029501585c8ebfba758753e0ac7c515
Greg Oliver <golive@microsoft.com>
2023-02-20 18:25:48 +0000

    pipeline

--- remote/setenv.sh
+++ remote/setenv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export TF_VAR_AZDO_ORG_NAME=${{ variables.azdo_org_name }}
-export TF_VAR_AZDO_PAT=${{ variables.azdo_pat }}
+export TF_VAR_AZDO_ORG_NAME=$azdo_org_name
+export TF_VAR_AZDO_PAT=$azdo_pat
