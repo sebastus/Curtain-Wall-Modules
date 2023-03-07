c136540cb0fa36d523af29e63bca276658671aa1
Greg Oliver <golive@microsoft.com>
2023-03-07 18:40:33 +0000

    fix references to context

--- linux-vm/metadata/invoke.template.tf
+++ linux-vm/metadata/invoke.template.tf
@@ -9,7 +9,7 @@ module "build-agent" {
 
   resource_group = module.rg_xxx.context_outputs.resource_group
   identity_ids   = [module.rg_xxx.context_outputs.managed_identity.id]
-  subnet_id      = module.rg_xxx.well_known_subnets["default"].id
+  subnet_id      = module.rg_xxx.context_outputs.well_known_subnets["default"].id
 
   include_azdo_ba         = true
   azdo_pat                = var.azdo_pat
@@ -19,10 +19,10 @@ module "build-agent" {
   azdo_pool_name          = var.xxx_ba_azdo_pool_name
   azdo_build_agent_name   = var.xxx_ba_azdo_build_agent_name
 
-  law_installed               = var.rg_xxx.context_outputs.log_analytics_workspace != null
+  law_installed               = (module.rg_xxx.context_outputs.log_analytics_workspace != null)
   install_omsagent            = true
-  log_analytics_workspace_id  = var.law_installed ? module.rg_xxx.context_outputs.log_analytics_workspace.workspace_id : null
-  log_analytics_workspace_key = var.law_installed ? module.rg_xxx.context_outputs.log_analytics_workspace.primary_shared_key : null
+  log_analytics_workspace_id  = (module.rg_xxx.context_outputs.log_analytics_workspace != null) ? module.rg_xxx.context_outputs.log_analytics_workspace.workspace_id : null
+  log_analytics_workspace_key = (module.rg_xxx.context_outputs.log_analytics_workspace != null) ? module.rg_xxx.context_outputs.log_analytics_workspace.primary_shared_key : null
 
   include_terraform = true
   terraform_version = "1.3.2"
