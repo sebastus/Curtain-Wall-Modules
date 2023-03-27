a30741d25f7f19965f97345d98f5d0b88f02ce17
Greg Oliver <golive@microsoft.com>
2023-03-27 16:11:18 +0100

    pass a subnet rather than an id store password in kv pass in pkr network use law_installed switch add some outputs

--- azdo-server/metadata/invoke.template.tf
+++ azdo-server/metadata/invoke.template.tf
@@ -12,16 +12,18 @@ module "azdo-server" {
   identity_ids = [
     module.tg_xxx.managed_identity == null ? null : module.tg_xxx.managed_identity.id
   ]
-  subnet_id = module.tg_xxx.well_known_subnets["default"].id
+  subnet = module.tg_xxx.well_known_subnets[var.xxx_azdo_subnet_name]
 
   # optionally install public ip
   create_pip = var.xxx_create_pip
 
-  # optionally install oms agent
+  key_vault = module.tg_xxx.azurerm_key_vault
+
+  law_installed    = module.tg_xxx.create_law
   install_omsagent = var.xxx_install_omsagent
 
-  log_analytics_workspace_id  = module.tg_xxx.log_analytics_workspace == null ? null : module.tg_xxx.log_analytics_workspace.workspace_id
-  log_analytics_workspace_key = module.tg_xxx.log_analytics_workspace == null ? null : module.tg_xxx.log_analytics_workspace.primary_shared_key
+  log_analytics_workspace_id  = module.tg_xxx.create_law ? module.tg_xxx.log_analytics_workspace.workspace_id : null
+  log_analytics_workspace_key = module.tg_xxx.create_law ? module.tg_xxx.log_analytics_workspace.primary_shared_key : null
 
   vhd_or_image = var.xxx_vhd_or_image
 
@@ -36,6 +38,10 @@ module "azdo-server" {
   arm_client_id          = var.xxx_arm_client_id
   arm_client_secret      = var.xxx_arm_client_secret
   arm_installer_password = var.xxx_arm_installer_password
+
+  pkr_vnet_rg_name = var.xxx_vnet_rg_name
+  pkr_vnet_name    = var.xxx_vnet_name
+  pkr_subnet_name  = var.xxx_subnet_name
 
   local_temp = var.xxx_local_temp
 
